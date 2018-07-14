92bb38cc97d287a395e4c6edca116cee6d302fdf
Karl <karolwieczorek9@o2.pl>
2018-07-14 22:31:18 +0200

    refactor

--- ScriptsTemplatesUpdater/Assets/CustomScriptsTemplateUpdater/Updater.cs
+++ ScriptsTemplatesUpdater/Assets/CustomScriptsTemplateUpdater/Updater.cs
@@ -2,33 +2,30 @@
 
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 
 namespace CustomScriptsTemplateUpdater
 {
-    public class Updater : EditorWindow
+    public partial class Updater : EditorWindow
     {
         const string NEW_TEMPLATES_PATH = "CustomScriptsTemplateUpdater/ScriptTemplates";
 
-        string TemplatesDirectory
+        static string TemplatesDirectory
         {
             get { return EditorApplication.applicationContentsPath + "/Resources/ScritpTemplates"; }
         }
 
         bool autoOpen; // TODO make as editor pref with default true
-        
-        bool updateScripts = true;
-        bool cleanUp = true;
-        bool restart = true;
 
-        [MenuItem("Tools/Restart")]
-        static void RestartUnity()
+        readonly List<UpdaterElement> elements = new List<UpdaterElement>
         {
-            Process.Start(EditorApplication.applicationPath); 
-            EditorApplication.Exit(0);
-        }
+            new UpdateScriptsElement(), 
+            new CleanUpElement(), 
+            new RestartElement()
+        };
 
         [MenuItem("Tools/LogPaths")]
         static void LogPaths()
@@ -49,16 +46,13 @@ namespace CustomScriptsTemplateUpdater
         {
             GUILayout.Label("Updater for Unity Script Templates");
 
-            DrawDescription();
+            foreach (var element in elements)
+                element.DrawDescription();
 
             if (GUILayout.Button(GetLabel()))
             {
-                if (updateScripts)
-                    UpdateScripts();
-                if (cleanUp)
-                    CleanUpProject();
-                if (restart)
-                    RestartUnity();
+                foreach (var element in elements)
+                    element.TryExecute();
 
                 return;
             }
@@ -69,47 +63,92 @@ namespace CustomScriptsTemplateUpdater
             GUILayout.EndHorizontal();
         }
 
-        void DrawDescription()
-        {
-            updateScripts = GUILayout.Toggle(updateScripts, "UpdateScripts");
-            var text = string.Format("Update scripts will copy all templates from \nAssets/{0} \nand paste them to \n{1}",
-                NEW_TEMPLATES_PATH, TemplatesDirectory);
-            GUILayout.Label(text);
-            
-            GUILayout.Space(10);
-
-            cleanUp = GUILayout.Toggle(cleanUp, "Clean Up");
-            var cleanUpText = string.Format("Clean up will remove {0}", "Assets/CustomScriptsTemplateUpdater");
-            GUILayout.Label(cleanUpText);
-            
-            GUILayout.Space(10);
-            
-            restart = GUILayout.Toggle(restart, "Restart");
-            var restartText = string.Format("Will restart unity.\nSo it will load new templates");
-            GUILayout.Label(restartText);
-        }
-
         string GetLabel()
         {
-            List<string> labelElements = new List<string>();
-            if (updateScripts)
-                labelElements.Add("Update Scripts");
-            if (cleanUp)
-                labelElements.Add("Clean Up Project");
-            if (restart)
-                labelElements.Add("Restart Unity");
-
-            return string.Join(" + ", labelElements.ToArray());
+            var labelElements = elements.Where(x => x.Enabled).Select(x => x.Name).ToArray();
+            return string.Join(" + ", labelElements);
         }
 
-        void UpdateScripts()
+        class UpdateScriptsElement : UpdaterElement
         {
-            
+            public override string Name
+            {
+                get { return "Update Scripts"; }
+            }
+
+            protected override string GetDescriptionText()
+            {
+                return string.Format("Update scripts will copy all templates from \nAssets/{0} \nand paste them to \n{1}",
+                    NEW_TEMPLATES_PATH, TemplatesDirectory);
+            }
+
+            protected override void Execute()
+            {
+                
+            }
         }
 
-        void CleanUpProject()
+        class RestartElement : UpdaterElement
         {
-            
+            public override string Name
+            {
+                get { return "Restart"; }
+            }
+
+            protected override string GetDescriptionText()
+            {
+                return "Will restart unity.\nSo it will load new templates";
+            }
+
+            protected override void Execute()
+            {
+                Process.Start(EditorApplication.applicationPath); 
+                EditorApplication.Exit(0);
+            }
+        }
+
+        class CleanUpElement : UpdaterElement
+        {
+            public override string Name
+            {
+                get { return "Clean Up"; }
+            }
+
+            protected override string GetDescriptionText()
+            {
+                return string.Format("Clean up will remove {0}", "Assets/CustomScriptsTemplateUpdater");
+            }
+
+            protected override void Execute()
+            {
+                Debug.Log(Name);
+            }
+        }
+
+        abstract class UpdaterElement
+        {
+            public abstract string Name { get; }
+            bool enabled = true;
+            public bool Enabled
+            {
+                get { return enabled; }
+            }
+
+            public void TryExecute()
+            {
+                if (enabled)
+                    Execute();
+            }
+
+            public void DrawDescription()
+            {
+                enabled = GUILayout.Toggle(enabled, Name);
+                GUILayout.Label(GetDescriptionText());
+                GUILayout.Space(10);
+            }
+
+            protected abstract string GetDescriptionText();
+            protected abstract void Execute();
         }
     }
 }
