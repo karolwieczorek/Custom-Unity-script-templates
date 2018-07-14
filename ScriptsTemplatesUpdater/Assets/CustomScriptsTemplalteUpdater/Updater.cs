bc396a11f00ca68981353999b277d19798e4a588
Karl <karolwieczorek9@o2.pl>
2018-07-14 21:20:53 +0200

    create unity project

---
+++ ScriptsTemplatesUpdater/Assets/CustomScriptsTemplalteUpdater/Updater.cs
@@ -0,0 +1,28 @@
+﻿#if UNITY_EDITOR
+
+using System;
+using System.Diagnostics;
+using UnityEditor;
+using Debug = UnityEngine.Debug;
+
+namespace CustomScriptsTemplalteUpdater
+{
+    public static class Updater
+    {
+        [MenuItem("Tools/Restart")]
+        static void RestartUnity()
+        {
+            Process.Start(EditorApplication.applicationPath); 
+            Environment.Exit(1);
+        }
+        
+        [MenuItem("Tools/LogPaths")]
+        static void LogPaths()
+        {
+            Debug.Log(EditorApplication.applicationPath);
+            Debug.Log(EditorApplication.applicationContentsPath);
+        }
+    }
+}
+
+#endif
